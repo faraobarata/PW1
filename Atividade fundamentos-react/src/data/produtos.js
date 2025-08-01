34e2cf9517098538155aefbfa48388f4e2568119
Pedro de almeida <141568199+faraobarata@users.noreply.github.com>
2025-08-01 15:27:36 -0300

    Add files via upload

---
+++ Atividade fundamentos-react/src/data/produtos.js
@@ -0,0 +1,11 @@
+export default [
+    { id: 1, nome: 'Arroz', preco: 6.20 },
+    { id: 2, nome: 'Feijão', preco: 7.60 },
+    { id: 3, nome: 'Macarrão', preco: 8.10 },
+    { id: 4, nome: 'Biscoito', preco: 5.70 },
+    { id: 5, nome: 'Leite', preco: 10.00 },
+    { id: 6, nome: 'Manteiga', preco: 9.50 },
+    { id: 7, nome: 'Pão de Forma', preco: 7.70 },
+    { id: 8, nome: 'Caneta', preco: 6.9034526 },
+    { id: 9, nome: 'Borracha', preco: 8.80 },
+]
